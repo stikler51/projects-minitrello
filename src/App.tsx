298004c2dfac852a2f453ecbe61bdad4e0845126
KirillZhdanov <kzhdanau@topcon.com>
2021-10-26 15:32:25 +0300

    Add InfiniteTable to table page

--- src/App.tsx
+++ src/App.tsx
@@ -1,102 +1,11 @@
-import React, { ReactElement, useState } from 'react'
-// import InfiniteTable from './components/InfiniteTable/InfiniteTable'
+import React, { ReactElement } from 'react'
 import './App.css'
-// import { PROJECTS } from './mocks'
-// import { ProjectsDataType } from './mocks/types'
-import { BrowserRouter as Router, Switch, Route, Link, useRouteMatch, useParams } from 'react-router-dom'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import { Home, Table, Trello } from './pages'
 
 function App(): ReactElement {
-  // const [hasNextPage, setHasNextPage] = useState<boolean>(true)
-  // const [isNextPageLoading, setIsNextPageLoading] = useState<boolean>(false)
-  // const [globalLoading, setGlobalLoading] = useState<boolean>(false)
-  // const [items, setItems] = useState<ProjectsDataType>([])
-
-  // const loadNextPage = (): void => {
-  //   setIsNextPageLoading(true)
-
-  //   // Case for real API
-
-  //   // fetch('https://url_to_api/projects')
-  //   //   .then((response) => response.json())
-  //   //   .then((data) => {
-  //   //     setHasNextPage(!!data.nextPage)
-  //   //     setItems([...items, ...data.items])
-  //   //     setIsNextPageLoading(false)
-  //   //   })
-
-  //   setTimeout(() => {
-  //     setHasNextPage(items.length < PROJECTS.length)
-  //     setItems([...items, ...PROJECTS.slice(items.length, items.length + 10)])
-  //     setIsNextPageLoading(false)
-  //   }, 1000)
-  // }
-
-  // const sortTableDataByRow = (fieldName: string, order: 'asc' | 'desc'): void => {
-  //   setGlobalLoading(true)
-  //   // Case for real API
-
-  //   // fetch('https://url_to_api/projects?sortBy=${fieldName}&sortOrder=${order}')
-  //   //   .then((response) => response.json())
-  //   //   .then((data) => {
-  //   //     setItems([...data.items])
-  //   //     setGlobalLoading(false)
-  //   //   })
-
-  //   setTimeout(() => {
-  //     const sortedArray = items.sort(function (a, b) {
-  //       if (order === 'desc') {
-  //         if (a.project_name > b.project_name) {
-  //           return 1
-  //         }
-  //         if (a.project_name < b.project_name) {
-  //           return -1
-  //         }
-  //       } else {
-  //         if (a.project_name < b.project_name) {
-  //           return 1
-  //         }
-  //         if (a.project_name > b.project_name) {
-  //           return -1
-  //         }
-  //       }
-
-  //       return 0
-  //     })
-  //     setGlobalLoading(false)
-  //     setItems([...sortedArray])
-  //   }, 1000)
-  // }
-
-  // const searchTableData = (searchPattern: string) => {
-  //   setGlobalLoading(true)
-  //   // Case for real API
-
-  //   // fetch('https://url_to_api/projects?projectName=${searchPattern}')
-  //   //   .then((response) => response.json())
-  //   //   .then((data) => {
-  //   //     setItems([...data.items])
-  //   //     setGlobalLoading(false)
-  //   //   })
-
-  //   setTimeout(() => {
-  //     const foundedRows = PROJECTS.filter((data) => data.project_name.includes(searchPattern))
-  //     setItems([...foundedRows])
-  //     setGlobalLoading(false)
-  //   }, 1000)
-  // }
-
   return (
     <div className="App container">
-      {/* <InfiniteTable
-        hasNextPage={hasNextPage}
-        isNextPageLoading={isNextPageLoading}
-        items={items}
-        sortData={sortTableDataByRow}
-        searchData={searchTableData}
-        loadNextPage={loadNextPage}
-        globalLoading={globalLoading}
-      /> */}
       <Router>
         <Switch>
           <Route path="/trello" exact>
