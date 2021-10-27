d348c6329ee5af1f879c5caa1c89c41b1c8f3245
KirillZhdanov <kzhdanau@topcon.com>
2021-10-27 15:52:25 +0300

    Add navigation

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,32 @@
 import React, { ReactElement } from 'react'
 import './App.css'
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom'
 import { Home, Table, Trello, Project } from './pages'
+
+const Navigation = () => {
+  return (
+    <div className="navigation">
+      <div className="container">
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/table">Table</Link>
+          </li>
+          <li>
+            <Link to="/trello">Mini Trello</Link>
+          </li>
+        </ul>
+      </div>
+    </div>
+  )
+}
 
 function App(): ReactElement {
   return (
     <Router>
+      <Navigation />
       <div className="App container">
         <Switch>
           <Route path="/trello" exact>
